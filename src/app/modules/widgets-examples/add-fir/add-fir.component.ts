89850340cff24c39b0b7a9bedc73e90763f5b487
Manimaranboss <manimaran@klabsindia.com>
2025-03-05 18:02:14 +0530

    changes : changes  add,edit

--- src/app/modules/widgets-examples/add-fir/add-fir.component.ts
+++ src/app/modules/widgets-examples/add-fir/add-fir.component.ts
@@ -116,7 +116,6 @@ export class AddFirComponent implements OnInit, OnDestroy {
   ];
   showOtherDesignation = false;
   otherDesignation: string = "";
-  showRelief = false;
   CaseHandledBy = [
     'Special Public Prosecutor',
     'Empanelled advocate',
@@ -177,7 +176,7 @@ i: number;
     this.initializeForm();
     this.firId = this.getFirIdFromSession(); // Get FIR ID from session storage
     this.loadOptions();
-    // this.loadOffenceActs();
+    this.loadOffenceActs();
     this.loadAccusedCommunities();
     // this.loadScstSections();
     this.generateYearOptions();
@@ -477,8 +476,8 @@ loadAllOffenceReliefDetails(): void {
 
 // Calls firService to update victim details based on selected offences
 onOffenceCommittedChange(event: any, index: number): void {
-  const selectedOffences = event.value; 
-  console.log(selectedOffences);
+  const selectedOffences = event.value; // Get selected values from the 30th field
+  console.log(this.victimsRelief);
   // const getId = selectedOffences.map((ele:any)=>ele.id);
   const getId = this.offenceOptions
     .filter(option => selectedOffences.includes(option.offence_name))
@@ -491,17 +490,6 @@ onOffenceCommittedChange(event: any, index: number): void {
     this.victimsRelief,
     getId
   );
-  selectedOffences.forEach((ele:any) => {
-    const selectedValue = ele.trim(); 
-    const validValues = ['Rape, etc., or unnatural Offences', 'Gang rape', 'Murder or Death'];
-  
-  if (validValues.includes(selectedValue)) {
-    this.showRelief = true;
-  }
-  else{
-    this.showRelief = false;
-  }
-  });
   this.cdr.detectChanges();
 }
 
@@ -1723,13 +1711,11 @@ onIsDeceasedChangeOutside(): void {
     // If "Yes", make the deceased person names field required
     deceasedPersonNamesControl?.setValidators([Validators.required]);
     deceasedPersonNamesControl?.enable();
-    this.showRelief = true;
   } else {
     // If "No", reset and disable the deceased person names field
     deceasedPersonNamesControl?.clearValidators();
     deceasedPersonNamesControl?.reset();
     deceasedPersonNamesControl?.disable();
-    this.showRelief = false;
   }
 
   deceasedPersonNamesControl?.updateValueAndValidity();
