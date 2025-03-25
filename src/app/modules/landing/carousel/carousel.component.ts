d60bb63e471e5a1ac655218cd6e57a5f331347d0
Manimaranboss <manimaran@klabsindia.com>
2025-03-25 11:37:49 +0530

    Revert "changes : Landing page changes"
    This reverts commit e001ed2a37b4c99f87a8ca3252fba64e15eab934.

--- src/app/modules/landing/carousel/carousel.component.ts
+++ src/app/modules/landing/carousel/carousel.component.ts
@@ -7,13 +7,13 @@ import { Component } from '@angular/core';
 })
 export class CarouselComponent {
   slides = [
-    { url: 'assets/images/adwt8.jpg', title: '', subtitle: '' },
-    { url: 'assets/images/adwt7.jpg', title: '', subtitle: '' },
-    { url: 'assets/images/adwt6.jpg', title: '', subtitle: '' },
-    // { url: 'assets/images/adwt1.jpg', title: '', subtitle: '' },
+    { url: 'assets/images/adwt1.jpg', title: '', subtitle: '' },
     { url: 'assets/images/adwt2.jpg', title: '', subtitle: '' },
-    { url: 'assets/images/adwt4.jpg', title: '', subtitle: '' },
     { url: 'assets/images/adwt3.jpg', title: '', subtitle: '' },
+    { url: 'assets/images/adwt4.jpg', title: '', subtitle: '' },
     { url: 'assets/images/adwt5.jpg', title: '', subtitle: '' },
+    { url: 'assets/images/adwt6.jpg', title: '', subtitle: '' },
+    { url: 'assets/images/adwt7.jpg', title: '', subtitle: '' },
+    { url: 'assets/images/adwt8.jpg', title: '', subtitle: '' },
   ];
 }
